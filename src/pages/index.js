74b2e796096235530e1fa6bdcabe0881fd4ebdbc
DumontGontran <dumont.gontran@orange.fr>
2023-05-24 16:29:02 +0200

    add index.js on pages to use imports better

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomeView } from '@pages/HomeView'
+export { default as AddingView } from '@pages/AddingView'
+export { default as EditingView } from '@pages/EditingView'
+export { default as ErrorView } from '@pages/ErrorView'
