105933115a2481300cc3b1e5cbfa88ecc2c5822c
DumontGontran <dumont.gontran@orange.fr>
2023-05-24 16:28:32 +0200

    update navigation routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '@pages/Home'
+import { HomeView, AddingView, ErrorView } from '@pages'
 import '@styles/base/app.scss'
 
 function App() {
@@ -7,12 +7,12 @@ function App() {
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route index path='/' element={<Home />}></Route>
-          <Route path='add' element={null}></Route>
-          <Route path='edit' element={null}></Route>
-          <Route path='delete' element={null}></Route>
+          <Route index path='/' element={<HomeView />}></Route>
+          <Route path='add' element={<AddingView />}></Route>
+          <Route path='list' element={null}></Route>
+          <Route path='wishlist' element={null}></Route>
           <Route path='favorites' element={null}></Route>
-          <Route path='/*' element={null}></Route>
+          <Route path='/*' element={<ErrorView />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
